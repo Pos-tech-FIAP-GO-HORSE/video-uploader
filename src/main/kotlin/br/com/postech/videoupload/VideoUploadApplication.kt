c6cc67cf55851600630f313310b05eaa22490970
Rafael RIbeiro <rafaelsouza.develop@gmail.com>
2025-04-16 19:51:29 -0300

    adicionada application class

--- src/main/kotlin/br/com/postech/videoupload/VideoUploadApplication.kt
+++ src/main/kotlin/br/com/postech/videoupload/VideoUploadApplication.kt
@@ -1,13 +1,10 @@
 package br.com.postech.videoupload
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
-import org.springframework.boot.autoconfigure.domain.EntityScan
 import org.springframework.boot.runApplication
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories
 
 @SpringBootApplication
-@EnableJpaRepositories("br.com.postech.videoupload.infra.persistence")
-@EntityScan("br.com.postech.videoupload.infra.persistence.entity")
+
 class VideoUploadApplication
 
 fun main(args: Array<String>) {
--- src/main/kotlin/br/com/postech/videoupload/infra/config/AppConfig.kt
+++ src/main/kotlin/br/com/postech/videoupload/infra/config/AppConfig.kt
@@ -1,11 +1,6 @@
 package br.com.postech.videoupload.infra.config
 
-import org.springframework.boot.autoconfigure.domain.EntityScan
-import org.springframework.context.annotation.ComponentScan
 import org.springframework.context.annotation.Configuration
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories
 
 @Configuration
-@EnableJpaRepositories("br.com.postech.videoupload.infra.persistence")
-@EntityScan("br.com.postech.videoupload.infra.persistence.entity")
 open class AppConfig
