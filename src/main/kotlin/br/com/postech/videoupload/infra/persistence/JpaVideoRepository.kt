40ab1a8d36f1be55f53724e18636685ded7523dc
Rafael RIbeiro <rafaelsouza.develop@gmail.com>
2025-04-16 19:13:29 -0300

    ajuste de anotação do JpaVideoRepository

--- src/main/kotlin/br/com/postech/videoupload/infra/persistence/JpaVideoRepository.kt
+++ src/main/kotlin/br/com/postech/videoupload/infra/persistence/JpaVideoRepository.kt
@@ -2,6 +2,8 @@ package br.com.postech.videoupload.infra.persistence
 
 import br.com.postech.videoupload.infra.persistence.entity.VideoEntity
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.stereotype.Repository
 import java.util.*
 
+@Repository
 interface JpaVideoRepository : JpaRepository<VideoEntity, UUID>
