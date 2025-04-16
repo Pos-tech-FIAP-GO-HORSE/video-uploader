0a1dfaa7f15e6a9ba8967368ab17c5b60cd04bdd
Rafael RIbeiro <rafaelsouza.develop@gmail.com>
2025-04-16 20:36:29 -0300

    adicionada application class

--- src/main/kotlin/br/com/postech/videoupload/VideoUploadApplication.kt
+++ src/main/kotlin/br/com/postech/videoupload/VideoUploadApplication.kt
@@ -1,11 +1,21 @@
 package br.com.postech.videoupload
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.builder.SpringApplicationBuilder
 import org.springframework.boot.runApplication
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer
+import org.springframework.cache.annotation.EnableCaching
+import org.springframework.scheduling.annotation.EnableAsync
 
 @SpringBootApplication
+@EnableAsync
+@EnableCaching
+class VideoUploadApplication : SpringBootServletInitializer(){
 
-class VideoUploadApplication
+    override fun configure(application: SpringApplicationBuilder): SpringApplicationBuilder {
+        return application.sources(VideoUploadApplication::class.java).lazyInitialization(true)
+    }
+}
 
 fun main(args: Array<String>) {
     runApplication<VideoUploadApplication>(*args)
